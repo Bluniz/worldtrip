df67836c9a5f0ee2e57b2982090c0558e4f06cfd
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-04-13 17:24:18 -0300

    Add initial Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+
+import { Banner } from "../components/Banner";
+import { Header } from "../components/Header";
+import { TravelTypes } from "../components/TravelTypes";
+
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <Flex w="100vw" h="100vh" direction="column">
+      <Header />
+      <Banner />
+      <TravelTypes />
+    </Flex>
+  );
 }
