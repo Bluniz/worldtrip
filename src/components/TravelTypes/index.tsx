10d8f5313afd1fbbe7ed28280f40b50b5224124d
Lucas Rosa <lucassantosrosa51@gmail.com>
2021-04-14 15:19:49 -0300

    Fix

--- src/components/TravelTypes/index.tsx
+++ src/components/TravelTypes/index.tsx
@@ -1,9 +1,26 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react";
 import { TravelItem } from "./TravelItem";
 
 export function TravelTypes() {
   return (
-    <Flex w="100%" align="center" justify="space-evenly">
+    <Flex
+      w="100%"
+      align="center"
+      justify="space-evenly"
+      pt={5}
+      paddingBottom={10}
+      position="relative"
+      _after={{
+        content: `""`,
+        position: "absolute",
+        bottom: 0,
+        left: "auto",
+        marginLeft: "20px",
+        backgroundColor: "gray.500",
+        height: "3px",
+        width: "90px",
+      }}
+    >
       <TravelItem src="/images/icons/cocktail.svg" title="vida noturna" />
       <TravelItem src="/images/icons/surf.svg" title="praia" />
       <TravelItem src="/images/icons/building.svg" title="moderno" />
